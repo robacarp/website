e2f3a81c335eb63837fb38ec941c96ddd79359df
Robert L. Carpenter <robert@robertcarpentr.net>
2010-05-23 21:38:35 -0600

    fixed environment file to correctly represent timezone.

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-#  config.time_zone = 'MST'
+  config.time_zone = 'Mountain Time (US & Canada)'
 end
