32440ec6aff5614cfca35d6586c4227b10558e35
Robert L. Carpenter <robert@robertcarpentr.net>
2010-05-23 21:36:46 -0600

    Content centered, fixing server config files

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-RAILS_ENV = "production"
 
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.time_zone = 'MST'
+#  config.time_zone = 'MST'
 end
