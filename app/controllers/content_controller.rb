1af6809196998913645d17c03280df0e2ea51ee1
Robert L. Carpenter <robacarp@gmail.com>
2010-06-02 05:27:36 +0000

    no idea what happened here...

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -5,19 +5,18 @@ class ContentController < ApplicationController
   def last
     @content = Content.find :last
   end
+
   def index
     content = Content.find :all
-    @archive = []
+    @archive = {}
     content.each do |c|
-      if !@archive[c.created_at.year] then
-        @archive[c.created_at.year] = []
+      if !@archive["#{c.created_at.year}"] then
+        @archive["#{c.created_at.year}"] = {}
       end
-      if !@archive[c.created_at.year][c.created_at.month] then
-        @archive[c.created_at.year][c.created_at.month] = []
+      if !@archive["#{c.created_at.year}"]["#{c.created_at.month}"] then
+        @archive["#{c.created_at.year}"]["#{c.created_at.month}"] = {}
       end
-      @archive[c.created_at.year][c.created_at.month] = c
+      @archive["#{c.created_at.year}"]["#{c.created_at.month}"] = c
     end
-
-
   end
 end
