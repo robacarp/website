ebdcebd8a68dbb6833f53570efae95230c5eda50
Robert L. Carpenter <robert@robertcarpentr.net>
2010-06-12 11:51:32 -0600

    Updated to not insert nil records when the ip address is unretrievable

--- app/controllers/ip_controller.rb
+++ app/controllers/ip_controller.rb
@@ -1,9 +1,13 @@
 class IpController < ApplicationController
   def index
     @ip = request.headers["HTTP_X_REAL_IP"]
-    ip = IpAddress.find_or_create_by_address @ip
-    ip.count += 1
-    ip.save
+    if @ip == '' then
+      @ip = nil
+    else 
+      ip = IpAddress.find_or_create_by_address @ip
+      ip.count += 1
+      ip.save
+    end
   end
 
   def list
