18fc4d0bf8f70f007c273a8b4655eb0ada5ea58f
Robert L. Carpenter <robacarp@gmail.com>
2010-11-06 12:04:22 -0600

    Ripping out RestfulAuthentication....looking for something simpler, wondering if devise will do what I want.

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem 'rails', '3.0.1'
 gem 'mysql', '2.8.1'
 gem 'ruby-debug'
 gem 'bluecloth', '>= 2.0.0'
+gem 'devise', '1.1.2'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
 module ApplicationHelper
-  # Format text for display.                                                                    
+  # Format text for display.
   def format(text)
     sanitize(markdown(text))
   end
 
-  # Process text with Markdown.                                                                 
+  # Process text with Markdown.
   def markdown(text)
     BlueCloth::new(text).to_html
+  end
+
+  def logged_in?
+    authenticate_or_request_with_http_basic do |user,password|
+      user=='robert' && password=='doom'
+    end
   end
 end
