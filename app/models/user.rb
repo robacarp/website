a6348e90a7c1d81c9c1d6629884c9db028c27f68
Robert L. Carpenter <robacarp@gmail.com>
2010-11-06 20:53:35 -0600

    password limits, registration block setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, :recoverable, :lockable
+
+  # interestingly, this works...if you restart the rails server ....
+  #if User.all.count == 0
+    devise :database_authenticatable, :registerable, :recoverable, :lockable
+  #else
+  #  devise :database_authenticatable, :recoverable, :lockable
+  #end
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  validates :password, :length=>{:minimum=>4}
 end
