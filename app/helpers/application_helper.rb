1500463968f2cc7920dfc71a7cd86ea56e5575a1
Robert L. Carpenter <robacarp@gmail.com>
2010-11-11 08:47:42 -0700

    learning about content_for...neat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,18 @@ module ApplicationHelper
     BlueCloth::new(text).to_html
   end
 
+  def title (text)
+    content_for(:title) { " :: #{text}" }
+  end
+
+  def javascript(*files)
+    content_for(:head) { javascript_include_tag(*files) }
+  end
+
+  def stylesheet(*files)
+    content_for(:head) { stylesheet_link_tag(*files) }
+  end
+
   def logged_in?
     authenticate_or_request_with_http_basic do |user,password|
       user=='robert' && password=='doom'
