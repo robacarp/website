57fb090d8ee42d09ff446a2ef9a32152863e4eee
Robert L. Carpenter <robacarp@gmail.com>
2010-11-17 00:16:56 -0700

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
 
-  # interestingly, this works...if you restart the rails server ....
-  #if User.all.count == 0
-  # devise :database_authenticatable, :registerable, :recoverable, :lockable
-  #else
-    devise :database_authenticatable, :recoverable, :lockable
-  #end
+  devise :database_authenticatable, :recoverable, :lockable#, :registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
